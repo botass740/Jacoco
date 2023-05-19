7b0c6606d8c7e2bebcba0d31a79017ceaaf18862
Max test <botass740@gmail.com>
2023-05-20 02:22:46 +0300

    JaCoCo

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class MaxServiceTest {
 
-   @Test
+    @Test
     public void shouldFindMax() {
         MaxService service = new MaxService();
 
@@ -13,6 +13,19 @@ public class MaxServiceTest {
         int expected = a;
         int actual = service.max(a, b);
 
-       Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfB() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 6;
+
+        int expected = b;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
